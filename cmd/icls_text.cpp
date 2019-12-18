53277856a83e8888b0c1001a6f1fa3236e13215e
J-Donald Tournier <jdtournier@gmail.com>
2019-12-18 15:08:27 +0000

    icls_text: add functinality for equality constraints

--- cmd/icls_text.cpp
+++ cmd/icls_text.cpp
@@ -16,8 +16,7 @@
 
 #include "command.h"
 #include "progressbar.h"
-#include "image.h"
-#include "algo/threaded_loop.h"
+#include "timer.h"
 #include "math/constrained_least_squares.h"
 
 
@@ -34,20 +33,20 @@ void usage ()
     + "i.e. solve for   MX = Y\n\n     such that   CX >= t";
 
   ARGUMENTS
-    + Argument ("input", "the input images Y.").type_image_in ()
-    + Argument ("problem", "the problem matrix M")
-    + Argument ("output", "the output solution image X.").type_image_out ();
+    + Argument ("problem", "the problem matrix M").type_file_in()
+    + Argument ("input", "the input vector Y.").type_file_in()
+    + Argument ("output", "the output solution vector X.").type_file_out ();
 
   OPTIONS
-
-    + Option ("mask", "only perform computation within the specified binary brain mask image.")
-    +   Argument ("image").type_image_in()
 
     + Option ("constraint", "specify C, the constraint matrix. By default, the algorithm will solve for a non-negative solution vector, and set this matrix to the identity.")
     +   Argument ("matrix").type_file_in()
 
     + Option ("threshold", "specify t, the constraint thresholds. By default, the algorithm will set this zero.")
     +   Argument ("matrix").type_file_in()
+
+    + Option ("num_equalities", "specify the number of constraints at the end of the contraint matrix/vector that should be treated as equalities (default: 0).")
+    +   Argument ("num").type_integer(0)
 
     + Option ("niter", "specify the maximum number of iterations to perform (default: 10 x num_parameters)")
     +   Argument ("num").type_integer (0)
@@ -59,55 +58,13 @@ void usage ()
     +   Argument ("value").type_float (0.0)
 
     + Option ("constraint_norm", "specify the regularisation to apply on the constraint vector norm - useful for poorly condition problems (default: 0.0)")
-    +   Argument ("value").type_float (0.0)
-
-    + Option ("prediction", "output predicted image")
-    +   Argument ("image").type_image_out();
+    +   Argument ("value").type_float (0.0);
 }
 
 
 
 
-typedef float value_type;
 typedef double compute_type;
-
-class Processor {
-  public:
-    Processor (const Math::ICLS::Problem<compute_type>& problem, Image<value_type>& prediction, const Eigen::VectorXd& threshold) :
-      solve (problem),
-      x(problem.H.cols()),
-      b(problem.H.rows()),
-      t (threshold),
-      prediction (prediction) { }
-
-    void operator() (Image<value_type>& in, Image<value_type>& out, Image<bool>& mask)
-    {
-      if (mask.valid() && !mask.value())
-        return;
-
-      for (auto l = Loop (3) (in); l; ++l)
-        b[in.index(3)] = in.value();
-
-      auto niter = solve (x, b, t);
-      if (niter >= solve.problem().max_niter)
-        INFO ("voxel at [ " + str(in.index(0)) + " " + str(in.index(1)) + " " + str(in.index(2)) + " ] failed to converge");
-
-      for (auto l = Loop (3) (out); l; ++l)
-        out.value() = x[out.index(3)];
-
-      if (prediction.valid()) {
-        assign_pos_of (in, 0, 3).to (prediction);
-        b = solve.problem().H * x;
-        for (auto l = Loop (3) (prediction); l; ++l)
-          prediction.value() = b[prediction.index(3)];
-      }
-    }
-
-    Math::ICLS::Solver<compute_type> solve;
-    Eigen::VectorXd x, b, t;
-    Image<value_type> prediction;
-};
-
 
 
 
@@ -118,9 +75,12 @@ void run ()
   auto tolerance           = get_option_value ("tolerance",       0.0);
   auto solution_norm_reg   = get_option_value ("solution_norm",   0.0);
   auto constraint_norm_reg = get_option_value ("constraint_norm", 0.0);
+  auto num_equalities      = get_option_value ("num_equalities",  0);
 
-  auto problem_matrix    = load_matrix<compute_type> (argument[1]);
+  auto problem_matrix    = load_matrix<compute_type> (argument[0]);
+  auto problem_vector    = load_vector<compute_type> (argument[1]);
   decltype (problem_matrix) constraint_matrix;
+  decltype (problem_vector) constraint_vector;
 
   auto opt = get_options ("constraint");
   if (opt.size()) {
@@ -133,39 +93,28 @@ void run ()
 
   Math::ICLS::Problem<compute_type> problem (problem_matrix, constraint_matrix, solution_norm_reg, constraint_norm_reg, max_iterations, tolerance);
 
-  Eigen::Matrix<compute_type, Eigen::Dynamic, 1> threshold;
   opt = get_options ("threshold");
   if (opt.size()) {
-    threshold = load_vector<compute_type> (opt[0][0]);
-    if (threshold.size() != problem.num_constraints())
+    constraint_vector = load_vector<compute_type> (opt[0][0]);
+    if (constraint_vector.size() != problem.num_constraints())
       throw Exception ("size of threshold vector does not match number of n constraint matrix");
   }
 
-  auto in = Image<value_type>::open (argument[0]);
-  if (in.size(3) != ssize_t (problem.num_measurements()))
-    throw Exception ("number of volumes in input image \"" + std::string (argument[0]) + "\" does not match number of columns in problem matrix \"" + std::string (argument[1]) + "\"");
+  Eigen::VectorXd x (problem_matrix.cols());
 
-  Image<bool> mask;
-  opt = get_options ("mask");
-  if (opt.size()) {
-    mask = Image<bool>::open (opt[0][0]);
-    check_dimensions (mask, in, 0, 3);
+  Timer timer;
+  Math::ICLS::Solver<compute_type> solve (problem);
+  auto niter = solve (x, problem_vector, constraint_vector, num_equalities);
+  auto elapsed = timer.elapsed();
+
+  if (niter >= solve.problem().max_niter) {
+    WARN ("failed to converge in " + str(niter) + " iterations (runtime: " + str(elapsed) + "s)");
+  } else {
+    CONSOLE ("converged in " + str(niter) + " iterations (runtime: " + str(elapsed) + "s)");
   }
 
-  opt = get_options ("prediction");
-  Image<value_type> prediction;
-  if (opt.size()) {
-    Header header = in;
-    header.datatype() = DataType::Float32;
-    prediction = Image<value_type>::create (opt[0][0], header);
-  }
-
-  Header header (in);
-  header.size (3) = problem.num_parameters();
-  auto out = Image<value_type>::create (argument[2], header);
-
-  ThreadedLoop ("performing constrained least-squares fit", in, 0, 3)
-    .run (Processor (problem, prediction, threshold), in, out, mask);
-
+  CONSOLE (str(x.transpose()));
+  save_vector (x, argument[2], KeyValues(), false);
 }
+
 
